fa6de517456cabdf06265d95677b31e3e72e43ff
Mahmoud Bassam <roys.msc2017@gmail.com>
2021-02-07 16:03:20 +0200

    added weather and favourites APIs

--- auth/middleware/basicAuth.js
+++ auth/middleware/basicAuth.js
@@ -1,20 +1,16 @@
 'use strict';
 
-const users = require('../schema/userSchema');
-const base64 = require('base-64');
+const weatherSecret = '1234';
+const jwt = require('jsonwebtoken');
+
+
 
 module.exports = (req,res,next)=>{
   if(!req.headers.authorization){
     next('Missing Basic Authorization Headers');
     return;
   }
-
   let basic = req.headers.authorization.split(' ').pop();
-  let [user,pass] = base64.decode(basic).split(':');
-  users.authenticateBasic(user,pass).then(validUser=>{
-    req.userDetails = validUser;
-    // req.token = users.generateToken(validUser);
-    next();
-  })
-    .catch(() => next('Wrong Username and/or Password'));
+  req.userData = jwt.verify(basic, weatherSecret);
+  next();
 };
